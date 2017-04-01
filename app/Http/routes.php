3bc0cdf2181c8f9d20ab86d06da1083e8cefbc85
gozaing <steganographi@gmail.com>
2017-04-01 21:53:12 +0900

    add auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,16 @@ Route::get('hello', function () {
     return 'Hello!';
 });
 
+// 認証ルート
+Route::get( 'auth/login',  'Auth\AuthController@getLogin');
+Route::post('auth/login',  'Auth\AuthController@postLogin');
+Route::get( 'auth/logout', 'Auth\AuthController@getLogout');
+
+// 登録ルート
+Route::get( 'auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+// タスクルート
+Route::get(   '/tasks', 'TaskController@index');
+Route::post(  '/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
