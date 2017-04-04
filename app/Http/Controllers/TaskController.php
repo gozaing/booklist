b1af3f9c5b92afd8d2e3a412311fc2c60347cfe4
gozaing <steganographi@gmail.com>
2017-04-04 22:38:53 +0900

    add tasks template

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -18,4 +18,9 @@ class TaskController extends Controller
     {
         $this->middleware('auth');
     }
+
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
 }
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+// resources/views/layouts/app.blade.php
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Intermediate</title>
+
+        <!-- CSSJavaScript -->
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- ナビバーの内容 -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
