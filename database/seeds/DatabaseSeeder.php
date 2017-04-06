3a5067b3f6fdb31a0be3a952edd06f8636ef8983
gozaing <steganographi@gmail.com>
2017-04-06 20:24:06 +0900

    add sample data of booklist, using seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
+use Carbon\Carbon;
+use App\Booklist;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,10 +17,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // EloquentのマスアサインメントをOFFにします
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
+        $this->call('BooklistTableSeeder');
 
+        // EloquentのマスアサインメントをONにします
         Model::reguard();
+    }
+}
+
+class BooklistTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('booklists')->delete();
+
+        $faker = Faker::create('en_US');
+
+        for ($i = 0; $i < 10; $i++) {
+            Booklist::create([
+                'user_id'    => 1,
+//                'title'      => $faker->sentences(),
+                'title'      => 'aaa',
+                'created_at' => Carbon::today(),
+            ]);
+        }
     }
 }
